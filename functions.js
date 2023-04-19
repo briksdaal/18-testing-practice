42cd8153c5eb83e69d59bbcd007df73bb09eca36
briksdaal <02250834+briksdaal@users.noreply.github.com>
2023-04-19 17:40:39 +0300

    Write tests for capitalize and the function itself

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
-function sum(a, b) {
-  return a + b;
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export default sum;
+export { capitalize };
--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,32 @@
-import sum from './functions.js';
+import { capitalize } from './functions.js';
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+describe('test capitalize function', () => {
+  test('capitalizes all lowercase string', () => {
+    expect(capitalize('asdfasdf')).toMatch('Asdfasdf');
+    expect(capitalize('milk')).toMatch('Milk');
+    expect(capitalize('telephone')).toMatch('Telephone');
+  });
+
+  test('already capitalized words are unchanged', () => {
+    expect(capitalize('Asdfasdf')).toMatch('Asdfasdf');
+    expect(capitalize('Kindergarten')).toMatch('Kindergarten');
+    expect(capitalize('Pizza')).toMatch('Pizza');
+  });
+
+  test('works on strings with spaces and special chars', () => {
+    expect(capitalize('i ate a pizza')).toMatch('I ate a pizza');
+    expect(capitalize('play it! it should work #_#')).toMatch('Play it! it should work #_#');
+    expect(capitalize('I\'m already capitalized')).toMatch('I\'m already capitalized');
+  });
+
+  test('works on empty strings', () => {
+    expect(capitalize('')).toMatch('');
+  });
+
+  test('works on one char strings', () => {
+    expect(capitalize('a')).toMatch('A');
+    expect(capitalize('aB')).toMatch('B');
+    expect(capitalize('4')).toMatch('4');
+    expect(capitalize('^')).toMatch('^');
+  });
 });
